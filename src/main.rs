77e2fb90a2d6b277a9599df6e8737db626b876c9
Craig Mayhew <craig@mayhew.io>
2024-01-24 14:54:40 +0000

    Refactored into args_setup() and args_read(). Added max_tokens and top_p arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{arg, command};
+use clap::{arg, command, Command};
 use colored::Colorize;
 use openai_api_rust::{
     Auth,
@@ -10,12 +10,14 @@ use openai_api_rust::{
 use reqwest;
 use std::io::{self, Read};
 
-fn initialise_chat_body (conversation_messages: Vec<Message>) -> ChatBody {
+/// initialise a ChatBody struct.
+/// Todo: This can probably be part of a lrger refactor so we aren't passing so many tuples back and forther between functions. i.e. we have ChatBody, just use that
+fn initialise_chat_body (max_tokens: i32, temperature: f32, top_p: f32, conversation_messages: Vec<Message>) -> ChatBody {
     ChatBody {
         model: "gpt-4".to_string(),
-        max_tokens: Some(4096),
-        temperature: Some(0.6_f32),
-        top_p: Some(0.95_f32),
+        max_tokens: Some(max_tokens),
+        temperature: Some(temperature),
+        top_p: Some(top_p),
         n: Some(1),
         stream: Some(false),
         stop: None,
@@ -27,19 +29,21 @@ fn initialise_chat_body (conversation_messages: Vec<Message>) -> ChatBody {
     }
 }
 
-async fn send_to_gpt4(input: &str, prepend: &str) -> Result<String, reqwest::Error> {
+/// send request to openai api
+async fn send_to_gpt4(input: &str, arguments: (String, i32, f32, f32)) -> Result<String, reqwest::Error> {
+    let (prepend, max_tokens, temperature, top_p) = arguments;
     let mut conversation_messages = vec![
         Message { role: Role::System, content: "You are a helpful assistant.".to_string() },
     ];
-    if !prepend.is_empty() {
-        conversation_messages.push(Message { role: Role::User, content: prepend.to_string() });
+    if !&prepend.is_empty() {
+        conversation_messages.push(Message { role: Role::User, content: prepend });
     }
     conversation_messages.push(Message { role: Role::User, content: "the following is piped input from the command line".to_owned() + input });
     // Load API key from environment OPENAI_API_KEY.
     // You can also hadcode through `Auth::new(<your_api_key>)`, but it is not recommended.
     let auth = Auth::from_env().unwrap();
     let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
-    let body = initialise_chat_body(conversation_messages);
+    let body = initialise_chat_body(max_tokens, temperature, top_p, conversation_messages);
     let rs = openai.chat_completion_create(&body);
     let choice = rs.unwrap().choices;
     let message = &choice[0].message.as_ref().unwrap();
@@ -47,27 +51,57 @@ async fn send_to_gpt4(input: &str, prepend: &str) -> Result<String, reqwest::Err
     Ok(message.content.to_string())
 }
 
-#[tokio::main]
-async fn main() {
-    let matches = command!() // requires `cargo` feature
-        .version("0.1")
-        .author("Craig Mayhew")
-        .about("Sends piped content to GPT-4")
+/// define command line argument and their descriptions
+fn args_setup() -> Command {
+    command!() // requires `cargo` feature
+        .about("Sends piped content to GPT-4. Author: Craig Mayhew")
         .arg(
             arg!(
-                -p [prepend] "Text to prepend to the piped content e.g. \"find the pattern\""
+                -p [prepend] "Text to prepend to the piped content e.g. \"find the pattern: \""
             )
             .required(false)
         )
-        .get_matches();
+        .arg(
+            arg!(
+                -t [temperature] "Advanced: Adjust temperature of response between 0.0 and 1.0. The higher the value, the more likely the generated text will be diverse, but there is a higher possibility of grammar errors and generation of nonsense."
+            )
+            .required(false)
+        )
+        .arg(
+            arg!(
+                -m [max_tokens] "Advanced: Adjust token limit up to a maximum of 4096 for GPT4."
+            )
+            .required(false)
+        ).arg(
+            arg!(
+                -s [top_p] "Advanced: Adjust top_p of response between 0.0 and 1.0. It's the nucleus sampling parameter."
+            )
+            .required(false)
+        )
+}
 
+/// parse command line arguments, setting to defaults where ommitted
+fn args_read (args_setup: Command) -> (std::string::String, i32, f32, f32) {
+    let matches = args_setup.get_matches();
+    
     let empty_string = String::from("");
+
     let prepend = matches.get_one::<String>("prepend").unwrap_or(&empty_string);
+    let max_tokens = matches.get_one::<i32>("max_tokens").unwrap_or(&4096);
+    let temperature = matches.get_one::<f32>("temperature").unwrap_or(&0.6);
+    let top_p = matches.get_one::<f32>("top_p").unwrap_or(&0.95);
+    
+    (prepend.to_owned(),max_tokens.to_owned(),temperature.to_owned(),top_p.to_owned())
+}
+
+#[tokio::main]
+async fn main() {
+    let parsed_arguments = args_read(args_setup());
 
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).expect("Failed to read from stdin");
 
-    match send_to_gpt4(&input, prepend).await {
+    match send_to_gpt4(&input, parsed_arguments).await {
         Ok(response) => println!("{}", response.green()),
         Err(e) => eprintln!("Error: {}", e),
     }
