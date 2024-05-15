c28bf837f46cd659a16a0887500a269f42e98b09
Craig Mayhew <craig@mayhew.io>
2024-05-15 12:33:20 +0100

    feat: gpt4 -> gpt4o by default

--- src/config/models.rs
+++ src/config/models.rs
@@ -1,5 +1,5 @@
-/// Defines which gpt model to use. Currently set to "gpt-4"
-pub const MODEL: &str = "gpt-4";
+/// Defines which gpt model to use. Currently set to "gpt-4o"
+pub const MODEL: &str = "gpt-4o";
 /// Defines default maximum number of tokens available in conversation and response
 pub const MAX_TOKENS: &i32 = &4096;
 /// Defines default temperature of response
